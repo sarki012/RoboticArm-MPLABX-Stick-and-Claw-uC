4918b268af57e8a2800bf9af940b3dadbacb2bc2
sarki012 <60797267+sarki012@users.noreply.github.com>
2023-04-14 14:11:47 -0500

    DMA and ADC work

---
+++ init.c
@@ -0,0 +1,207 @@
+/*
+ * File:   init.c for UC2
+ * Author: Erik Sarkinen
+ *
+ * Created on February 12, 2022, 8:26 AM
+ */
+
+#include <xc.h>
+#include <stdio.h>
+#include <p33ep512mc502.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void init(void)
+{ 
+    int i = 0;
+    OSCCONbits.NOSC0 = 0;       //Fast RC Oscillator
+    OSCCONbits.NOSC1 = 0;       //Fast RC Oscillator
+    OSCCONbits.NOSC2 = 0;       //Fast RC Oscillator
+    OSCCONbits.OSWEN = 1;       //Requests oscillator switch to selection specified by the NOSC[2:0] bits
+    OSCTUN = 0;         //Center frequency 7.37 MHz
+ //   CLKDIVbits.PLLPOST = 0;     //Output divided by 2
+   // CLKDIVbits.PLLPRE = 0;      //Input divided by 2
+    //PLLFBD = 48;                 //50 is the PLL multiplier
+    RPINR18 = 0b0000000000100110;       //U1RX = RP38
+    RPINR19 = 0b0000000000100101;       //U2RX = RP37 
+    RPOR3 = 0b0000001100000001;     // U2TX = RP41, U1_TX = RP40
+
+   // Servo Circuit Board Config 
+    TRISBbits.TRISB4 = 0;       //EAN
+    TRISBbits.TRISB1 = 0;       //P2_4
+    TRISBbits.TRISB0 = 0;       //P2_0
+    TRISAbits.TRISA4 = 0;       //SW Button
+    //Set up Bluetooth  EEPROM programming   Normal Normal_NC2
+    LATBbits.LATB4 = 0;     //EAN  0           0        1     
+    LATBbits.LATB1 = 1;     //P2_4 1           1        1
+    LATBbits.LATB0 = 1;     //P2_0 0           1        1 
+    LATAbits.LATA4 = 1;     //SW Button
+    
+    ///////////////////UART2/////////////////////////////////////////////
+    U2MODEbits.UARTEN = 0;      //UART2 is disabled
+    TRISBbits.TRISB9 = 1;       //U2 TX
+    TRISBbits.TRISB5 = 1;       //U2 RX
+    U2MODEbits.USIDL = 0;       //Continue module operation in Idle mode
+    U2MODEbits.IREN = 0;        //IrDA encoder and decoder disabled
+    U2MODEbits.UEN0 = 0;        //U2TX and RX pins are enabled and used. 
+    U2MODEbits.UEN1 = 0;        //CTS and RTS pins are controlled by PORT latches
+    U2MODEbits.WAKE = 0;        //UART2 continues to sample the U1RX pin; interrupt is generated on the falling edge
+    U2MODEbits.LPBACK = 0;      //Loopback mode is disabled
+    U2MODEbits.ABAUD = 0;       //Auto-Baud disabled
+    U2MODEbits.URXINV = 0;      //Idle state is '0'
+    U2MODEbits.BRGH = 1;        //High Speed
+    U2MODEbits.PDSEL1 = 0;      //8-bit data, no parity
+    U2MODEbits.URXINV = 0;  // Bit4 IdleState = 1  (for dsPIC)
+    U2MODEbits.RTSMD = 0;   // Bit11 Simplex Mode
+    U2MODEbits.PDSEL0 = 0;      
+    U2MODEbits.STSEL = 0;       //One stop bit 
+    INTCON2bits.GIE = 1;        //Global interrupt enable bit (enabled)
+    IEC1bits.U2RXIE = 1;        //UART 2 interrupt enabled
+    IFS1bits.U2RXIF = 0;       //Reset RX interrupt flag 
+    IPC7bits.U2RXIP = 7;        //Set RX interrupt priority to high priority
+    U2STAbits.URXISEL1 = 0;     //Interrupt is set when there is 1 char
+    U2STAbits.URXISEL0 = 0;     
+    U2STAbits.ADDEN = 0;          //Address detect mode disabled
+    U2STAbits.PERR = 0;         //Parity Error
+    U2STAbits.OERR = 0;           //Clear the Receive //Overflow Flag.
+    U2BRG = 7;                 //BAUD RATE 115,200 Fosc = 7.37 Mhz, Fp = Fosc/2 5 is close
+    IEC1bits.U2TXIE = 0;        //No transmit interrupt (We poll TRMT)
+    U2MODEbits.UARTEN = 1;      //UART2 is enabled
+    U2STAbits.UTXEN = 1;        //Transmit enabled
+   
+    ///////////////////UART1/////////////////////////////////////////////
+    U1MODEbits.UARTEN = 0;      //UART1 is disabled
+    TRISBbits.TRISB6 = 1;       //U1 RX
+    TRISBbits.TRISB8 = 1;       //U1 TX
+    U1MODEbits.USIDL = 0;       //Continue module operation in Idle mode
+    U1MODEbits.IREN = 0;        //IrDA encoder and decoder disabled
+    U1MODEbits.UEN0 = 0;        //U1TX and RX pins are enabled and used. 
+    U1MODEbits.UEN1 = 0;        //CTS and RTS pins are controlled by PORT latches
+    U1MODEbits.WAKE = 0;        //UART1 continues to sample the U1RX pin; interrupt is generated on the falling edge
+    U1MODEbits.LPBACK = 0;      //Loopback mode is disabled
+    U1MODEbits.ABAUD = 0;       //Auto-Baud disabled
+    U1MODEbits.URXINV = 0;      //Idle state is '0'
+    U1MODEbits.BRGH = 1;        //High Speed
+    U1MODEbits.PDSEL1 = 0;      //8-bit data, no parity
+    U1MODEbits.URXINV = 0;      // Bit4 IdleState = 1  (for dsPIC)
+    U1MODEbits.RTSMD = 0;       // Bit11 Simplex Mode
+    U1MODEbits.PDSEL0 = 0;      
+    U1MODEbits.STSEL = 0;       //One stop bit 
+    IEC0bits.U1RXIE = 1;        //UART 1 interrupt enabled
+    IFS0bits.U1RXIF = 0;        //Reset RX interrupt flag 
+    IPC2bits.U1RXIP = 7;        //Set RX interrupt priority to high priority
+    U1STAbits.URXISEL1 = 0;     //Interrupt is set when there is 1 char
+    U1STAbits.URXISEL0 = 0;     
+    U1STAbits.ADDEN = 0;        //Address detect mode disabled
+    U1STAbits.PERR = 0;         //Parity Error
+    U1STAbits.OERR = 0;         //Clear the Receive //Overflow Flag.
+    U1BRG = 7;                 //BAUD RATE 9,600 Fosc = 7.37 Mhz, Fp = Fosc/2
+    IEC0bits.U1TXIE = 0;        //No transmit interrupt (We poll TRMT)
+    U1MODEbits.UARTEN = 1;      //UART1 is enabled
+    U1STAbits.UTXEN = 1;        //Transmit enabled
+    
+    ////////////////PWM Initialization///////////////////////////////
+    PTCONbits.PTEN = 0;         //PWM Module is disabled before any code
+    
+    TRISBbits.TRISB15 = 0;      //PWM1L is an output
+    TRISBbits.TRISB14 = 0;      //PWM1H is an output
+    TRISBbits.TRISB13 = 0;      //PWM2L is an output
+    TRISBbits.TRISB12 = 0;      //PWM2H is an output
+    TRISBbits.TRISB11 = 0;      //PWM3L is an output
+    TRISBbits.TRISB10 = 0;      //PWM3H is an output different types of outputs?
+    PORTBbits.RB15 = 0;         //Clear the output pin
+    PORTBbits.RB14 = 0;         //Clear the output pin
+    PORTBbits.RB13 = 0;         //Clear the output pin
+    PORTBbits.RB12 = 0;         //Clear the output pin
+    PORTBbits.RB11 = 0;         //Clear the output pin
+    PORTBbits.RB10 = 0;         //Clear the output pin
+    
+    PTCONbits.SYNCOEN = 0;      //SYNCO1 output is enabled
+    PTCONbits.SYNCEN = 0;       //External synchronization of primary time base is disabled
+    PTCONbits.SYNCSRC0 = 0;     //Synchronous Source is PTGO17
+    PTCONbits.SYNCSRC1 = 1;
+    PTCONbits.SYNCSRC2 = 0;
+    PTCON2bits.PCLKDIV0 = 0;      //010 divide by 4 prescaler *Adjust this depending on resolution
+    PTCON2bits.PCLKDIV1 = 1;
+    PTCON2bits.PCLKDIV2 = 0;
+    PTCONbits.PTSIDL = 0;
+    PTCONbits.EIPU = 0;
+    PTCONbits.SYNCPOL = 0;
+    PWMCON1bits.ITB = 1;        //PHASE1 register provides timing for this PWM generator
+    PWMCON2bits.ITB = 1;        //PHASE2 register provides timing for this PWM generator
+    PWMCON3bits.ITB = 1;        //PHASE3 register provides timing for this PWM generator
+    PWMCON1bits.MDCS = 0;       //PDC1 register provide duty cycle information for this PWM generator
+    PWMCON2bits.MDCS = 0;       //PDC2 register provide duty cycle information for this PWM generator
+    PWMCON3bits.MDCS = 0;       //PDC3 register provide duty cycle information for this PWM generator
+    PWMCON1bits.FLTIEN = 0;     //Fault interrupt disabled
+    PWMCON2bits.FLTIEN = 0;     //Fault interrupt disabled
+    PWMCON1bits.TRGIEN = 0;     //Trigger interrupt disabled
+    PWMCON2bits.TRGIEN = 0;     //Trigger interrupt disabled
+    PWMCON1bits.CLIEN = 0;      //Current limit interrupt disabled
+    PWMCON2bits.CLIEN = 0;      //Current limit interrupt disabled
+    PWMCON1bits.MTBS = 0;       //Primary master time base
+    PWMCON2bits.MTBS = 0;       //Primary master time base
+    PWMCON1bits.CAM = 0;        //Edge aligned
+    PWMCON2bits.CAM = 0;        //Edge aligned
+    PWMCON3bits.CAM = 0;        //Edge aligned
+    PWMCON1bits.FLTSTAT = 0;    //No fault interrupt is pending
+    PWMCON2bits.FLTSTAT = 0;    //No fault interrupt is pending
+    PWMCON1bits.DTC = 0;
+    PWMCON2bits.DTC = 0;
+    PWMCON1bits.XPRES = 0;
+    PWMCON2bits.XPRES = 0;
+    PWMCON1bits.IUE = 0;
+    PWMCON2bits.IUE = 0;
+    PWMCON1bits.DTC = 2;
+    PWMCON2bits.DTC = 2;
+    IOCON1bits.PENL = 1;        //PWM module controls PWM1L pin
+    IOCON2bits.PENL = 1;        //PWM module controls PWM2L pin
+    IOCON1bits.PENH = 1;        //PWM module controls PWM1H pin
+    IOCON2bits.PENH = 1;        //PWM module controls PWM2H pin
+    IOCON3bits.PENH = 1;        //PWM module controls PWM3H pin
+    IOCON3bits.PENL = 1;        //PWM module controls PWM3L pin
+    IOCON1bits.PMOD0 = 1;       //Pin pair is in the redundant output mode
+    IOCON1bits.PMOD1 = 0;       //Pin pair is in the redundant output mode
+    IOCON2bits.PMOD0 = 1;       //Pin pair is in the redundant output mode
+    IOCON2bits.PMOD1 = 0;       //Pin pair is in the redundant output mode
+    IOCON3bits.PMOD0 = 1;       //Pin pair is in the redundant output mode
+    IOCON3bits.PMOD1 = 0;       //Pin pair is in the redundant output mode
+    IOCON1bits.OVRENH = 0;      //PWM generator controls pin
+    IOCON1bits.OVRENL = 0;      //PWM generator controls pin
+    IOCON2bits.OVRENH = 0;      //PWM generator controls pin
+    IOCON2bits.OVRENL = 0;      //PWM generator controls pin
+    IOCON3bits.OVRENL = 0;      //PWM generator controls pin
+    IOCON3bits.OVRENH = 0;      //PWM generator controls pin
+    IOCON1bits.POLH = 0;
+    IOCON1bits.POLL = 0;
+    IOCON2bits.POLH = 0;
+    IOCON2bits.POLL = 0;
+    IOCON3bits.POLH = 0;
+    IOCON3bits.POLL = 0;
+    IOCON1bits.SWAP = 0;
+    IOCON2bits.SWAP = 0;
+    IOCON3bits.SWAP = 0;
+    IOCON1bits.OSYNC = 0;
+    IOCON2bits.OSYNC = 0;
+    IOCON3bits.OSYNC = 0;
+    IOCON1bits.OVRDAT = 0;
+    IOCON2bits.OVRDAT = 0;
+    IOCON3bits.OVRDAT = 0;
+    PTCONbits.SEIEN = 0;        //Special event interrupt disabled
+    DTR1 = 0;
+    DTR2 = 0;
+    ALTDTR1 = 0;
+    ALTDTR2 = 0;
+    FCLCON1 = 0x0000;
+    FCLCON2 = 0x0000;
+    FCLCON1bits.FLTMOD = 3;
+    FCLCON2bits.FLTMOD = 3;
+    PTCONbits.PTEN = 1;         //PWM Module is enabled
+    
+
+     
+
+    
+    return;
+}
---
+++ main.c
@@ -0,0 +1,152 @@
+/*
+ * File:   main.c
+ * Author: Erik Sarkinen
+ *
+ * Created on May 5, 2022, 1:46 PM
+ */
+// 'C' source line config statements
+
+// FICD
+#pragma config ICS = PGD1               // ICD Communication Channel Select bits (Communicate on PGEC1 and PGED1)
+#pragma config JTAGEN = OFF             // JTAG Enable bit (JTAG is disabled)
+
+// FPOR
+#pragma config ALTI2C1 = OFF            // Alternate I2C1 pins (I2C1 mapped to SDA1/SCL1 pins)
+#pragma config ALTI2C2 = OFF            // Alternate I2C2 pins (I2C2 mapped to SDA2/SCL2 pins)
+#pragma config WDTWIN = WIN25           // Watchdog Window Select bits (WDT Window is 25% of WDT period)
+
+// FWDT
+#pragma config WDTPOST = PS32768        // Watchdog Timer Postscaler bits (1:32,768)
+#pragma config WDTPRE = PR128           // Watchdog Timer Prescaler bit (1:128)
+#pragma config PLLKEN = ON              // PLL Lock Enable bit (Clock switch to PLL source will wait until the PLL lock signal is valid.)
+#pragma config WINDIS = OFF             // Watchdog Timer Window Enable bit (Watchdog Timer in Non-Window mode)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable bit (Watchdog timer enabled/disabled by user software)
+
+// FOSC
+#pragma config POSCMD = NONE            // Primary Oscillator Mode Select bits (Primary Oscillator disabled)
+#pragma config OSCIOFNC = ON            // OSC2 Pin Function bit (OSC2 is general purpose digital I/O pin)
+#pragma config IOL1WAY = OFF            // Peripheral pin select configuration (Allow multiple reconfigurations)
+#pragma config FCKSM = CSDCMD           // Clock Switching Mode bits (Both Clock switching and Fail-safe Clock Monitor are disabled)
+
+// FOSCSEL
+#pragma config FNOSC = FRC              // Oscillator Source Selection (Internal Fast RC (FRC))
+#pragma config PWMLOCK = OFF            // PWM Lock Enable bit (PWM registers may be written without key sequence)
+#pragma config IESO = ON                // Two-speed Oscillator Start-up Enable bit (Start up device with FRC, then switch to user-selected oscillator source)
+
+// FGS
+#pragma config GWRP = OFF               // General Segment Write-Protect bit (General Segment may be written)
+#pragma config GCP = OFF                // General Segment Code-Protect bit (General Segment Code protect is Disabled)
+
+#include <xc.h>
+#include <p33ep512mc502.h>
+/* Standard includes. */
+#include <stdio.h>
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "croutine.h"
+#include "FreeRTOSConfig.h"
+#include "main.h"
+
+
+volatile char usbRxval[20];     //The UART receive array which holds the data sent 
+                                //via USB from the Raspberry Pi
+volatile char rxval[20];    
+int x = 0, y = 0;
+unsigned int stickADC = 0, tipADC = 0, clawADC = 0;
+
+//Raspberry Pi USB to UART receive interrupt
+void __attribute__((__interrupt__, auto_psv)) _U1RXInterrupt(void)             
+{
+    IFS0bits.U1RXIF = 0;        //Clear the interrupt flag
+    usbRxval[x] = U1RXREG;         //Add the value in the receive register to the receive array
+    x++;
+    if(x == 20)
+    {  
+        x = 0;
+    }
+    return;
+ }
+
+void __attribute__((__interrupt__, auto_psv)) _U2RXInterrupt(void)             
+{
+    IFS1bits.U2RXIF = 0;        //Clear the interrupt flag
+    rxval[y] = U2RXREG;         //Add the value in the receive register to the receive array
+    y++;
+    if(y == 20)
+    {  
+        y = 0;
+    }
+    return;
+ }
+/*=============================================================================
+_DMA0Interrupt(): ISR name is chosen from the device linker script.
+=============================================================================*/
+
+unsigned int DmaBuffer = 0;
+void __attribute__((interrupt, no_auto_psv)) _DMA0Interrupt(void)
+{
+	if(DmaBuffer == 0)
+	{
+	//	ProcessADCSamples(&BufferA[0][0]);
+	//	ProcessADCSamples(&BufferA[1][0]);
+	//	ProcessADCSamples(&BufferA[2][0]);
+	}
+	else
+	{
+	//	ProcessADCSamples(&BufferB[4][0]);
+	//	ProcessADCSamples(&BufferB[5][0]);
+	//	ProcessADCSamples(&BufferB[10][0]);
+	//	ProcessADCSamples(&BufferB[13][0]);
+	}
+
+	DmaBuffer ^= 1;	
+	IFS0bits.DMA0IF = 0;		// Clear the DMA0 Interrupt Flag
+}
+void __attribute__((__interrupt__, auto_psv)) _DefaultInterrupt(void)
+{
+    if(IFS0bits.AD1IF)
+    {
+        /*
+        IFS0bits.AD1IF = 0;
+        stickADC = ADC1BUF0;
+        tipADC = ADC1BUF1;
+        clawADC = ADC1BUF2;
+     */
+       return;
+    }
+    else
+         return;
+    return;
+}
+
+void main(void) {
+    int i = 0;
+    for(i = 0; i < 20; i++)
+    {
+        usbRxval[i] = 0;
+        rxval[i] = 0;       //Initialize the receive array to all 0's
+    }
+    init();     //Setup clock, UART, and PWMs
+    initAdc1();
+    initTmr3();
+    initDma0();
+    //DMA0STAL = __builtin_dmaoffset(stickBuffer); //DMA0 start address low byte 
+    xTaskCreate( stickThread, "Stick", 512, NULL, 1, NULL );    //Thread that controls the stick
+	xTaskCreate( tipThread, "Tip", 512, NULL, 1, NULL );      //Thread that controls the tip motion
+    xTaskCreate( clawThread, "Claw", 512, NULL, 1, NULL );      //Thread that controls the tip motion
+//    xTaskCreate( feedbackThread, "Feedback", 512, NULL, 1, NULL );      //Thread that sends the feedback values
+    
+	//Start the scheduler
+	vTaskStartScheduler();
+
+	/* Will only reach here if there is insufficient heap available to start
+	the scheduler. */
+    return;
+}
+void vApplicationIdleHook( void )
+{
+	/* Schedule the co-routines from within the idle task hook. */
+	vCoRoutineSchedule();
+}
